4475b44deca5c816bf62d86969ae7811e3cfe5f8
fedEnder <lemmer.j.christopher@gmail.com>
2019-10-28 22:56:43 -0500

    Specified key was too long error - 'https://laravel-news.com/laravel-5-4-key-too-long-error'

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,11 +4,11 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>{{ config('app.name') }}</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
+    <link rel="stylesheet" href="{{ public_path('css/styles.css') }}">
         <!-- Styles -->
         <style>
             html, body {
